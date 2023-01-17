7019cff7461ce44e29f5028ac255b84e821c2463
Ariam Salazar <ariamsalazar@gmail.com>
2023-01-17 15:36:28 -0400

    New project embutidos

---
+++ embutidos-gray-reygran/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: [
+		'plugin:react/recommended',
+		'standard-with-typescript',
+		'plugin:react/jsx-runtime',
+		'eslint-config-prettier',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: 'tsconfig.eslint.json',
+		tsconfigRootDir: './',
+	},
+	plugins: ['react'],
+	rules: {},
+};
---
+++ embutidos-gray-reygran/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,tsx,ts}'],
+	theme: {
+		extend: {},
+		colors: {
+			white: '#ffff',
+			black: '#0000',
+			red: '#D50000',
+			gray: '#9F9F9E',
+			'red-dark': '#A10010',
+			'yellow-dark': '#D29308',
+			'gray-dark': '#1E1E1E',
+			'gray-light': '#C5C0C0',
+		},
+	},
+	plugins: [],
+};
